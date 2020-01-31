30328da7ade9af1529a6b1ae7784c66a4fab1136
deskara <morganjgorle@gmail.com>
2020-01-31 19:10:06 +0000

    Made the camera follow the player

---
+++ BuildaBot/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    GameObject player;
+    GameObject Camera;
+    Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        Camera.transform.position = newPosition;
+        
+    }
+}
