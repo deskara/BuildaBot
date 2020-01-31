ede6a6e82ec1277b8f3d2f83c3d67187aaca6977
deskara <morganjgorle@gmail.com>
2020-01-31 17:54:52 +0000

    Added jump functionality

--- BuildaBot/Assets/Scripts/characterController.cs
+++ BuildaBot/Assets/Scripts/characterController.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
+    GameObject player;
+    Rigidbody2D characterBody;
+    Vector2 jumpVector;
+
+    void Start()
+    {
+        characterBody = GetComponent<Rigidbody2D>();
+    }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        jumpVector = new Vector2(0, 500);
         if (Input.GetKeyDown("space")){
+            characterBody.AddForce(jumpVector);
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
 
         }
-        
     }
 }
