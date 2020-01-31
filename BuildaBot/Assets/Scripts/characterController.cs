6aae1025878233f0e5c385b3c7843c84fc710f90
deskara <morganjgorle@gmail.com>
2020-01-31 17:37:59 +0000

    Solved collision with platforms

---
+++ BuildaBot/Assets/Scripts/characterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space")){
+
+        }
+        
+    }
+}
