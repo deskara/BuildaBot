e2c089c463524302ebcd84cc4bd1dbdec33574a7
deskara <morganjgorle@gmail.com>
2020-01-31 18:09:50 +0000

    Prevented the player from infinitely jumping

--- BuildaBot/Assets/Scripts/characterController.cs
+++ BuildaBot/Assets/Scripts/characterController.cs
@@ -6,19 +6,40 @@ public class characterController : MonoBehaviour
 {
     GameObject player;
     Rigidbody2D characterBody;
+    bool grounded;
 
 
     void Start()
     {
+        grounded = true;
         characterBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //This checks all collisions, if they have the platform tag, the player is set to grounded
+        if (collision.gameObject.tag == "Platform")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //This checks all collision exits, if they used to have the platform tag then the player is no longer grounded
+        if (collision.gameObject.tag == "Platform")
+        {
+            grounded = false;
+        }
+
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (Input.GetKey("space")){
+        //If the user presses space and they are grounded the player goes up into the air.
+        if (Input.GetKey("space") && grounded == true){
             Vector2 jumpVector;
             jumpVector = new Vector2(0, 250);
             characterBody.AddForce(jumpVector);
